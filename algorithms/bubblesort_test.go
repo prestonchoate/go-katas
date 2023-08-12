31dcfeccdd7181f530522a62261b79f9d351e1d8
Preston Choate <pchoate15@gmail.com>
2023-08-11 20:27:35 -0400

    moved bubblesort test to the correct namespace

--- algorithms/bubblesort_test.go
+++ algorithms/bubblesort_test.go
@@ -1,15 +1,13 @@
-package main
+package algorithms
 
 import (
 	"reflect"
 	"testing"
-
-	"github.com/prestonchoate/go-katas/algorithms"
 )
 
 func TestBubbleSort(t *testing.T) {
 	nums := []int{1, 4, 23, 65, 213, 4, 0, 5}
-	sorted := algorithms.BubbleSort(nums)
+	sorted := BubbleSort(nums)
 
 	expected := []int{0, 1, 4, 4, 5, 23, 65, 213}
 
